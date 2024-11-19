908ba576cfc76f33baff4e95236f258aad9c4bcc
TH4YSZ <thainacristina.sz07@gmail.com>
2024-11-19 16:23:08 -0300

    Correção de erros, estilização e redirecionamento de páginas.
    Co-authored-by: yRuanz <yRuanz@users.noreply.github.com>
    Co-authored-by: Mirella Turqueti <Allerim321@users.noreply.github.com>
    Co-authored-by: Yuri Azevedo Antunes de Souza  <Projectyuuri07@users.noreply.github.com>
    Co-authored-by: natinhaaa <natinhaaa@users.noreply.github.com>
    Co-authored-by: Alexandre Lemes <alemes7@users.noreply.github.com>

--- frequency_management/static/js/compressaoimg.js
+++ frequency_management/static/js/compressaoimg.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 const imagens = fs.readdirSync('img')
 
 imagens.forEach((imagem) => {
-    if(imagem.endsWith(".jpg")){
+    if (imagem.endsWith(".jpg")) {
         compressionImagem(imagem)
     }
 })
 
-async function compressionImagem(imagem){
+async function compressionImagem(imagem) {
     sharp(`img/${imagem}`)
-    .jpeg({quality: 50})
-    .resize(2000)
-    .toFile(`teste/${imagem}`)
+        .jpeg({ quality: 50 })
+        .resize(2000)
+        .toFile(`teste/${imagem}`)
 }
